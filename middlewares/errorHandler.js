fe3f052753b0479f39200afd65a5986e3f2ebea8
salamidrus <idrussalam95gmail.com>
2021-03-30 22:01:37 +0700

    Add Error Handler

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,20 @@
+module.exports = (err, req, res, next) => {
+  if (err) {
+    if (err.status) {
+      res.status(err.status).json({
+        success: false,
+        message: err.message,
+      });
+    } else if (err.response && err.response.data) {
+      res.status(400).json({
+        success: false,
+        message: err.response.data.error,
+      });
+    } else {
+      res.status(400).json({
+        success: false,
+        message: err.message,
+      });
+    }
+  }
+};
